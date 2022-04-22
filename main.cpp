4c04d6a18df2f24a319cd29f388aea9a674b0392
quang1409thanh <97092690+quang1409thanh@users.noreply.github.com>
2022-04-23 05:50:54 +0700

    commit bài 5 có thể di chuyển được cái paddle ở dưới

--- main.cpp
+++ main.cpp
@@ -14,11 +14,22 @@ int bkw=800;
 int bkh=600;
 int bkwmin=0;
 int bkhmin=0;
+
+int batx=bkw/2;
+int baty=bkh-22;
 void EnvenHandler(){
     // do something
     SDL_PollEvent(&event);
     if(event.type == SDL_QUIT){
         quit = true;
+    }
+    else if(event.type == SDL_KEYDOWN){
+        if(event.key.keysym.sym == SDLK_LEFT&&batx>0){
+            batx=batx-2;
+        }
+        else if(event.key.keysym.sym == SDLK_RIGHT&&batx<bkw-91){
+            batx=batx+2;
+        }
     }
 }
 
@@ -43,22 +54,27 @@ int main(int argc, char** argv){
                                           800, 600,
                                           SDL_WINDOW_SHOWN);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_Rect ballrect ={20,20,20,20};
+    
     SDL_Rect bkrect ={0,0,800,600};
-
+    
+    
     SDL_Surface *ball = IMG_Load("ball.png");
     SDL_Surface *bk = IMG_Load("bg.jpg");
-
+    SDL_Surface *bat = IMG_Load("batdone.png");
+    
     SDL_Texture *balltexture = SDL_CreateTextureFromSurface(renderer, ball);
     SDL_Texture *bktexture = SDL_CreateTextureFromSurface(renderer, bk);
-   
+    SDL_Texture *battexture = SDL_CreateTextureFromSurface(renderer, bat);
+
     while(!quit){
         EnvenHandler();
         SDL_Rect ballrect ={ballx,bally,20,20};
+        SDL_Rect batrect ={batx,baty,91,22};
         moveBall();
         ball_collision();
         SDL_RenderCopy(renderer, bktexture, NULL,&bkrect);
         SDL_RenderCopy(renderer, balltexture, NULL, &ballrect);
+        SDL_RenderCopy(renderer, battexture, NULL, &batrect);
         SDL_RenderPresent(renderer);
         SDL_RenderClear(renderer);
     }
