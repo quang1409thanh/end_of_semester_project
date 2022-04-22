035273ff61078827e0172f64bc6700c584789616
quang1409thanh <97092690+quang1409thanh@users.noreply.github.com>
2022-04-23 04:35:34 +0700

    lec 2 Displaying our background image

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 bool quit=false;
 
 SDL_Event event;
-
 void EnvenHandler(){
     // do something
     SDL_PollEvent(&event);
@@ -17,12 +16,19 @@ int main(int argc, char** argv){
     SDL_Window *window = SDL_CreateWindow("My Game",
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
-                                          640, 480,
+                                          800, 600,
                                           SDL_WINDOW_SHOWN);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_Rect ballrect ={20,20,20,20};
+    SDL_Rect bkrect ={0,0,800,600};
+
     SDL_Surface *ball = IMG_Load("ball.png");
+    SDL_Surface *bk = IMG_Load("bg.jpg");
+
     SDL_Texture *balltexture = SDL_CreateTextureFromSurface(renderer, ball);
+    SDL_Texture *bktexture = SDL_CreateTextureFromSurface(renderer, bk);
+
+    SDL_RenderCopy(renderer, bktexture, NULL,&bkrect);
     SDL_RenderCopy(renderer, balltexture, NULL, &ballrect);
     SDL_RenderPresent(renderer);
     while(!quit){
